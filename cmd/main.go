eb2541ae997640c5c4397795c8cea9118593eca3
pol-cova <83374367+pol-cova@users.noreply.github.com>
2024-07-15 10:34:44 -0600

    Prepare for deploy

--- cmd/main.go
+++ cmd/main.go
@@ -4,13 +4,18 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/pol-cova/go-url-shortener-api/db"
 	"github.com/pol-cova/go-url-shortener-api/routes"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 	app := echo.New()
 	db.InitDB()
 	routes.Router(app)
-	err := app.Start(":8080")
+	err := app.Start(":" + port)
 	if err != nil {
 		panic("could not start the server")
 	}
